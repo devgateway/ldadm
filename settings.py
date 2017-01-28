658e5ea526ee46e9229c70f5eea42f918bc92ec7
Styopa Semenukha <ssemenukha@developmentgateway.org>
2017-01-28 02:09:47 -0500

    Boilerplate for settings

--- settings.py
+++ settings.py
@@ -1,3 +1,15 @@
 import yaml
+import os
+import sys
 
 class Settings:
+    def __init__(self):
+        try:
+            cfg_dir = os.environ["XDG_CONFIG_HOME"]
+        except KeyError:
+            cfg_dir = os.environ["HOME"] + "/.config"
+
+        try:
+            cfg_file = open(cfg_dir + "/ldadm.yml")
+        except OSError as err:
+            sys.exit("Config file '%s': %s" % (err.filename, err.strerror))
