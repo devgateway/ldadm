77e35acb4de3195cae0e8df0dff04b99c9991d27
Styopa Semenukha <ssemenukha@developmentgateway.org>
2017-02-07 14:14:34 -0500

    Added key commands

--- ldadm.sh
+++ ldadm.sh
@@ -1,5 +1,6 @@
 _ldadm() {
-	local cur obj cmd
+	local cur prev obj cmd active suspended
+	_init_completion || return
 	COMPREPLY=()
 	cur="${COMP_WORDS[COMP_CWORD]}"
 	obj="${COMP_WORDS[1]}"
@@ -9,7 +10,7 @@ _ldadm() {
 		user)
 			case "$cmd" in
 				list|search|find)
-					if [ $COMP_CWORD -eq 3 ]; then
+					if [[ $COMP_CWORD -eq 3 ]]; then
 						COMPREPLY=($(compgen -W "--suspended" -- $cur))
 					fi
 					;;
@@ -27,12 +28,30 @@ _ldadm() {
 					COMPREPLY=($(compgen -W "$active $suspended" -- $cur))
 					;;
 				rename)
-					if [ $COMP_CWORD -eq 3 ]; then
+					if [[ $COMP_CWORD -eq 3 ]]; then
 						active=$(ldadm user list)
 						COMPREPLY=($(compgen -W "$active" -- $cur))
 					fi
 					;;
 				key)
+					local keycmd="${COMP_WORDS[3]}"
+					case "$keycmd" in
+						add|create|delete|remove)
+							prev="${COMP_WORDS[COMP_CWORD - 1]}"
+							if [[ "$prev" == "-f" || "$prev" == "--file" ]]; then
+								_filedir
+							else
+								active=$(ldadm user list)
+								COMPREPLY=($(compgen -W "-f --file $active" -- $cur))
+							fi
+							;;
+						list|show)
+							active=$(ldadm user list)
+							COMPREPLY=($(compgen -W "$active" -- $cur))
+							;;
+						*)
+							COMPREPLY=($(compgen -W "add create delete remove list show" -- $cur))
+					esac
 					;;
 				*)
 					COMPREPLY=($(compgen -W "list search find show info suspend ban lock
