51476a12989febf81db5567f96eff1c7278ff73d
Styopa Semenukha <ssemenukha@developmentgateway.org>
2017-02-22 17:53:24 -0500

    Wrote boilerplate for abstract objects

--- directory/directory.py
+++ directory/directory.py
@@ -7,3 +7,16 @@ import ldap3
 
 from .config import cfg
 from .ldap import ldap
+
+class DirectoryMapping(MutableMapping):
+    def __init__(self, attrs = None):
+        self._attrs = attrs
+
+class DirectoryObject:
+    def __init__(self, id):
+        self._id = id
+        self._dn = None
+        self._attrs = {}
+
+    def __str__(self):
+        return self._id
