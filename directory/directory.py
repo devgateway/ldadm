235aa37abbee26a3a89289d334970d407fa318e4
Styopa Semenukha <ssemenukha@developmentgateway.org>
2017-02-22 17:54:41 -0500

    Searching for single and multiple items

--- directory/directory.py
+++ directory/directory.py
@@ -12,6 +12,36 @@ class DirectoryMapping(MutableMapping):
     def __init__(self, attrs = None):
         self._attrs = attrs
 
+    def _getitem(self, id, attrs = None):
+        ldap.search(
+                search_base = __class__._base,
+                search_filter = "(%s=%s)" % (__class__._id_attr, id),
+                search_scope = __class__._scope,
+                attributes = attrs,
+                size_limit = 1)
+
+        if ldap.response:
+            return ldap.response[0]
+        else:
+            raise IndexError(id)
+
+    def search(self, filter):
+        return ldap.extend.standard.paged_search(
+                search_base = __class__._base,
+                search_filter = filter,
+                search_scope = self._scope,
+                attributes = self._attrs)
+
+    def __contains__(self, id):
+        try:
+            self.__getitem__(id, attrs = None)
+            return True
+        except IndexError:
+            return False
+
+    def __getitem__(self, id):
+        self._getitem(id, attrs = self._attrs)
+
 class DirectoryObject:
     def __init__(self, id):
         self._id = id
