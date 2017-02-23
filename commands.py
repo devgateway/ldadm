63c7a926203319918e694244226e0b2c3b667a45
Styopa Semenukha <ssemenukha@developmentgateway.org>
2017-02-23 12:33:08 -0500

    Removed features implemented elsewhere

--- commands.py
+++ commands.py
@@ -7,22 +7,6 @@ import ldap3
 
 import settings
 
-def scope(scope_str):
-    scopes = {
-            "base": ldap3.BASE,
-            "one": ldap3.LEVEL,
-            "sub": ldap3.SUBTREE
-            }
-    try:
-        return scopes[scope_str.lower()]
-    except KeyError as key:
-        msg = "Scope must be %s, not '%s'" % ("|".join(scopes), scope_str)
-        raise ValueError(msg) from key
-
-class NotFound(Exception):
-    def log(self):
-        logging.error( str(self) )
-
 class Command:
     def __init__(self, args):
         self._args = args
@@ -56,31 +40,6 @@ class Command:
                 for line in stdin:
                     yield line[:-1]
 
-    def _move_entry(self, dn, new_superior):
-        rdn = "+".join( ldap3.utils.dn.safe_rdn(dn) )
-        logging.debug("Moving %s to %s, keeping RDN %s" % (dn, new_superior, rdn))
-        self._ldap.modify_dn(
-                dn = dn,
-                relative_dn = rdn,
-                new_superior = new_superior)
-
-    def _rename_entry(self, dn, attr, new_val):
-        rdns = ldap3.utils.dn.safe_rdn(dn, decompose = True)
-        new_rdns = []
-        for rdn in rdns:
-            if rdn[0] == attr:
-                new_rdns.append( (rdn[0], new_val) )
-            else:
-                new_rdns.append(rdn)
-
-        new_rdn = "+".join(new_rdns)
-        logging.debug("Renaming %s to RDN %s" % (dn, new_rdn))
-        self._ldap.modify_dn(dn = dn, relative_dn = new_rdn)
-
-    def _delete_entry(self, dn):
-        logging.debug("Deleting %s" % dn)
-        self._ldap.delete(dn)
-
     def _input_entry(self, objectclass, templates):
         if self._args.template:
             entry = self._get_single_entry(self._args.template, attrs = ldap3.ALL_ATTRIBUTES)
@@ -96,49 +55,7 @@ class Command:
                 prompt = "%s [%s]: " % (attr_def.key
                 val = input(prompt)
 
-    def _add_entry(self, dn, objectclass, attrs):
-        logging.debug("Adding %s" % dn)
-        self._ldap.add(dn, objectclass, attrs)
-        # TODO: check exit status
-
 class UserCommand(Command):
-    def _search_users(self, filt):
-        user = self._cfg.user
-        if self._args.suspended:
-            base = user.base.suspended
-        else:
-            base = user.base.active
-
-        logging.debug("Search '%s' in '%s' scope %s" % (filt, base, user.scope))
-        entries = self._ldap.extend.standard.paged_search(
-                search_base = base,
-                search_filter = filt,
-                search_scope = scope(user.scope),
-                attributes = user.attr.uid)
-        for entry in entries:
-            print(entry["attributes"][user.attr.uid][0])
-
-    def _get_single_entry(self, username, active = True, attrs = None):
-        user = self._cfg.user
-        filt = "(%s=%s)" % (user.attr.uid, username)
-        if active:
-            base = user.base.active
-        else:
-            base = user.base.suspended
-
-        logging.debug("Search '%s' in '%s' scope %s" % (filt, base, user.scope))
-        generator = self._ldap.search(
-                search_base = base,
-                search_filter = filt,
-                search_scope = scope(user.scope),
-                attributes = attrs,
-                size_limit = 1)
-
-        if self._ldap.response:
-            return self._ldap.response[0]
-        else:
-            raise NotFound("User %s not found" % username)
-
     def _get_unique_id_number(self):
         user = self._cfg.user
         umin = user.nuid.min
