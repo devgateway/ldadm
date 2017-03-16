beb883d7aefe2ee746e49b976dee9499d28ee822
Styopa Semenukha <ssemenukha@developmentgateway.org>
2017-03-16 12:45:23 -0400

    Moved user completion to its own function

--- ldadm.sh
+++ ldadm.sh
@@ -1,81 +1,107 @@
 _ldadm() {
-	local cur prev obj cmd active suspended
 	_init_completion || return
 	COMPREPLY=()
-	cur="${COMP_WORDS[COMP_CWORD]}"
-	obj="${COMP_WORDS[1]}"
-	cmd="${COMP_WORDS[2]}"
 
-	case "$obj" in
+	case "${COMP_WORDS[1]}" in
 		user)
-			case "$cmd" in
-				list|search|find)
-					if [[ $COMP_CWORD -eq 3 ]]; then
-						COMPREPLY=($(compgen -W "--suspended" -- $cur))
-					fi
-					;;
-				show|info|suspend|ban|lock|disable)
-					active=$(ldadm user list)
-					COMPREPLY=($(compgen -W "$active" -- $cur))
-					;;
-				restore|unban|enable)
-					suspended=$(ldadm user list --suspended)
-					COMPREPLY=($(compgen -W "$suspended" -- $cur))
-					;;
-				delete|remove)
-					active=$(ldadm user list)
-					suspended=$(ldadm user list --suspended)
-					COMPREPLY=($(compgen -W "$active $suspended" -- $cur))
-					;;
-				add|create)
-					case "$COMP_CWORD" in
-						3)
-							COMPREPLY=($(compgen -W "-t --template $active" -- $cur))
-							;;
-						4)
-							active=$(ldadm user list)
-							COMPREPLY=($(compgen -W "$active" -- $cur))
-							;;
-					esac
-					;;
-				rename)
-					if [[ $COMP_CWORD -eq 3 ]]; then
-						active=$(ldadm user list)
-						COMPREPLY=($(compgen -W "$active" -- $cur))
-					fi
-					;;
-				key)
-					local keycmd="${COMP_WORDS[3]}"
-					case "$keycmd" in
-						add|create|delete|remove)
-							prev="${COMP_WORDS[COMP_CWORD - 1]}"
-							if [[ "$prev" == "-f" || "$prev" == "--file" ]]; then
-								_filedir
-							else
-								active=$(ldadm user list)
-								COMPREPLY=($(compgen -W "-f --file $active" -- $cur))
-							fi
-							;;
-						list|show)
-							active=$(ldadm user list)
-							COMPREPLY=($(compgen -W "$active" -- $cur))
-							;;
-						*)
-							COMPREPLY=($(compgen -W "add create delete remove list show" -- $cur))
-					esac
-					;;
-				*)
-					COMPREPLY=($(compgen -W "list search find show info suspend ban lock
-					disable restore unban enable delete remove add create rename key
-					" -- $cur))
-					;;
-			esac
+			_ldadm_user
 			;;
 		list)
 			;;
 		*)
 			COMPREPLY=($(compgen -W "user list" -- $cur))
 			return 0
+			;;
+	esac
+}
+
+_ldadm_user() {
+	local cur users
+	cur="${COMP_WORDS[COMP_CWORD]}"
+	case "${COMP_WORDS[2]}" in
+		list|search|find)
+			if [[ $COMP_CWORD -eq 3 ]]; then
+				COMPREPLY=($(compgen -W "--suspended" -- $cur))
+			fi
+			;;
+		info|show)
+			case "$COMP_CWORD" in
+				3)
+					case "$cur" in
+						-*)
+							COMPREPLY=($(compgen -W "--suspended" -- $cur))
+							return 0
+							;;
+						*)
+							users=$(ldadm user list)
+							COMPREPLY=($(compgen -W "$users" -- $cur))
+							return 0
+							;;
+					esac
+					;;
+				4)
+					if [[ "${COMP_WORDS[3]}" = "--suspended" ]]; then
+						users=$(ldadm user list --suspended)
+					else
+						users=$(ldadm user list)
+					fi
+					COMPREPLY=($(compgen -W "$users" -- $cur))
+			esac
+			;;
+		suspend|ban|lock|disable)
+			users=$(ldadm user list)
+			COMPREPLY=($(compgen -W "$users" -- $cur))
+			;;
+		restore|unban|enable)
+			users=$(ldadm user list --suspended)
+			COMPREPLY=($(compgen -W "$users" -- $cur))
+			;;
+		delete|remove)
+			users=$(ldadm user list --suspended)
+			COMPREPLY=($(compgen -W "$users" -- $cur))
+			;;
+		add|create)
+			users=$(ldadm user list)
+			case "$COMP_CWORD" in
+				3)
+					COMPREPLY=($(compgen -W "-t --template $users" -- $cur))
+					;;
+				4)
+					users=$(ldadm user list)
+					COMPREPLY=($(compgen -W "$users" -- $cur))
+					;;
+			esac
+			;;
+		rename)
+			if [[ $COMP_CWORD -eq 3 ]]; then
+				users=$(ldadm user list)
+				COMPREPLY=($(compgen -W "$users" -- $cur))
+			fi
+			;;
+		key)
+			local prev
+			local keycmd="${COMP_WORDS[3]}"
+			users=$(ldadm user list)
+			case "$keycmd" in
+				add|create|delete|remove)
+					prev="${COMP_WORDS[COMP_CWORD - 1]}"
+					if [[ "$prev" == "-f" || "$prev" == "--file" ]]; then
+						_filedir
+					else
+						COMPREPLY=($(compgen -W "-f --file $users" -- $cur))
+					fi
+					;;
+				list|show)
+					COMPREPLY=($(compgen -W "$users" -- $cur))
+					;;
+				*)
+					COMPREPLY=($(compgen -W "add create delete remove list show" -- $cur))
+			esac
+			;;
+		*)
+			COMPREPLY=($(compgen -W "list search find show info suspend ban lock
+			disable restore unban enable delete remove add create rename key
+			" -- $cur))
 			;;
 	esac
 }
