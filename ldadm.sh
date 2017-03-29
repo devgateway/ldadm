ca3bf2c0bebc7d81540e4bdafa19ebe3c590a586
Styopa Semenukha <ssemenukha@developmentgateway.org>
2017-03-28 23:56:51 -0400

    Updated shell completion

--- ldadm.sh
+++ ldadm.sh
@@ -3,13 +3,10 @@ _ldadm() {
 	local COMMAND='LOG_LEVEL=CRITICAL ldadm'
 	local KWD_OBJECTS="user list unit"
 	local KWD_SUSPENDED="--suspended"
-	local KWD_LOGLEVEL="--loglevel"
 	local KWD_DEFAULTS="--defaults"
 	local KWD_FILE="--file"
 	local KWD_FULL="--full"
 	local KWD_PARENT="--parent"
-	local OBJ_START
-	local PREV
 	local REPLY
 	local COMPLETION_LIB=
 	if [[ -n "$(type _init_completion 2>/dev/null)" ]]; then
@@ -17,32 +14,13 @@ _ldadm() {
 		COMPLETION_LIB=y
 	fi
 
-	# detect index of object, such as user, list, etc
-	if [[ "${COMP_WORDS[1]}" = "$KWD_LOGLEVEL" ]]; then
-		OBJ_START=3
-	else
-		OBJ_START=1
-	fi
-
 	# call per-object completion
-	case "${COMP_WORDS[OBJ_START]}" in
+	case "${COMP_WORDS[1]}" in
 		user) __ldadm_complete_user ;;
 		list) __ldadm_complete_list ;;
 		unit) __ldadm_complete_unit ;;
-		*)    __ldadm_complete_default ;;
+		*) COMPREPLY=($(compgen -W "$KWD_OBJECTS" -- $CUR)) ;;
 	esac
-}
-
-# complete log levels or objects
-__ldadm_complete_default() {
-	case $COMP_CWORD in
-		1) REPLY="$KWD_LOGLEVEL $KWD_OBJECTS" ;;
-		2) REPLY="DEBUG INFO WARNING ERROR CRITICAL" ;;
-		3) REPLY="$KWD_OBJECTS" ;;
-		*) REPLY=""
-	esac
-
-	COMPREPLY=($(compgen -W "$REPLY" -- $CUR))
 }
 
 # list active or suspended users
@@ -60,11 +38,9 @@ __ldadm_list_units() {
 
 # complete user commands
 __ldadm_complete_user() {
-	# adjust COMP_CWORD for easier indexing, skip loglevel args
-	let COMP_CWORD=(COMP_CWORD - OBJ_START + 1)
 	local USERS
 
-	case "${COMP_WORDS[OBJ_START + 1]}" in
+	case "${COMP_WORDS[2]}" in
 		list|search|find)
 			if [[ $COMP_CWORD -eq 3 ]]; then
 				REPLY="$KWD_SUSPENDED"
@@ -78,7 +54,7 @@ __ldadm_complete_user() {
 					*)  REPLY="$KWD_SUSPENDED $(__ldadm_list_users)" ;;
 				esac
 			else
-				if [[ "${COMP_WORDS[OBJ_START + 2]}" = "$KWD_SUSPENDED" ]]; then
+				if [[ "${COMP_WORDS[3]}" = "$KWD_SUSPENDED" ]]; then
 					REPLY="$(__ldadm_list_users suspended)"
 				else
 					REPLY="$(__ldadm_list_users)"
@@ -104,9 +80,8 @@ __ldadm_complete_user() {
 			fi
 			;;
 		key)
-			local PREV
 			USERS=$(__ldadm_list_users)
-			case "${COMP_WORDS[OBJ_START + 2]}" in
+			case "${COMP_WORDS[3]}" in
 				add|create)
 					case $COMP_CWORD in
 						4) REPLY="$KWD_FILE" ;;
@@ -146,10 +121,7 @@ __ldadm_complete_list() {
 
 # complete unit commands
 __ldadm_complete_unit() {
-	# adjust COMP_CWORD for easier indexing, skip loglevel args
-	let COMP_CWORD=(COMP_CWORD - OBJ_START + 1)
-
-	case "${COMP_WORDS[OBJ_START + 1]}" in
+	case "${COMP_WORDS[2]}" in
 		list) ;;
 		add)
 			case "$COMP_CWORD" in
