8caad0a778309a1e91ca1f87da8b8d020c8d6a22
Styopa Semenukha <ssemenukha@developmentgateway.org>
2017-04-07 16:27:49 -0400

    Wrote boilerplate for project command

---
+++ ldadm/project.py
@@ -0,0 +1,123 @@
+import logging
+
+from ldap3.core.exceptions import LDAPEntryAlreadyExistsResult, LDAPKeyError, \
+        LDAPAttributeOrValueExistsResult
+
+from .command import Command
+from .collections import ProjectMapping, UserMapping, MissingObjects
+from .config import cfg
+from .objects import Project
+from .parsers import single_user, multi_user, single_project, multi_project
+
+log = logging.getLogger(__name__)
+
+class ProjectCommand(Command):
+    __base = cfg.project.base
+    parser_name = "project"
+    parser_args = {
+        "kwargs": {
+            "help": "Projects"
+        },
+        "subparsers_title": "Project command",
+        "subparsers": {
+            "list": {
+                "kwargs": {
+                    "help": "List all projects"
+                }
+            },
+            "show": {
+                "kwargs": {
+                    "parents": [multi_project],
+                    "aliases": ["info"],
+                    "help": "List project attributes"
+                }
+            },
+            "add": {
+                "kwargs": {
+                    "aliases": ["create"],
+                    "help": "Add a project"
+                },
+                "arguments": {
+                    "--defaults": {
+                        "dest": "defaults",
+                        "metavar": "PROJECT",
+                        "nargs": 1,
+                        "help": "Suggest defaults from an existing project"
+                    }
+                }
+            },
+            "delete": {
+                "kwargs": {
+                    "parents": [multi_project],
+                    "aliases": ["remove"],
+                    "help": "Delete projects"
+                }
+            },
+            "assign": {
+                "kwargs": {
+                    "parents": [single_project, multi_user],
+                    "help": "Make users members of the project"
+                }
+            },
+            "manage": {
+                "kwargs": {
+                    "parents": [single_project, single_user],
+                    "help": "Make user the manager of the project"
+                }
+            }
+        }
+    }
+
+    def _get_project(self, project_name):
+        projects = ProjectMapping(limit = [project_name])
+
+        try:
+            project_list = [p for p in projects]
+            return project_list[0]
+        except MissingObjects as err:
+            raise RuntimeError("Project %s not found" % project_name) from err
+
+    def on_project_list(self):
+        projects = ProjectMapping()
+        for name in projects.keys():
+            print(name)
+
+    def on_project_show(self):
+        project_names = list(self._args_or_stdin("project"))
+        projects = ProjectMapping(base = base, limit = project_names, attrs = ALL_ATTRIBUTES)
+        try:
+            for project_entry in projects:
+                pretty_print(project_entry)
+        except MissingObjects as err:
+            raise RuntimeError("Projects not found: " + ", ".join(err.items))
+
+    def on_project_add(self):
+        attr_name = ProjectMapping._attribute
+
+        # Get default values from a reference object
+        if self._args.defaults:
+            source_obj = self._get_project(self._args.defaults, ALL_ATTRIBUTES)
+        else:
+            source_obj = None
+
+        project = Project(reference_object = source_obj)
+        id = project.attrs[attr_name]
+        projects = ProjectMapping()
+        projects[id] = project.attrs
+
+        if project.message:
+            print(project.message)
+
+    def on_project_delete(self):
+        project_names = list(self._args_or_stdin("project"))
+        if not project_names:
+            return
+
+        projects = ProjectMapping(limit = project_names)
+        for name in project_names:
+            del projects[name]
+
+        try:
+            projects.commit_delete()
+        except LDAPNotAllowedOnNotLeafResult as err:
+            raise RuntimeError("One or more projects not empty") from err
