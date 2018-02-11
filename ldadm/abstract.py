5e54308e8a83852ee06fdf1f9ce85cdcbb77be6b
Styopa Semenukha <ssemenukha@developmentgateway.org>
2018-02-10 20:42:26 -0500

    Copied some code from ldap3 library

--- ldadm/abstract.py
+++ ldadm/abstract.py
@@ -13,7 +13,7 @@ except ImportError:
 
 from ldap3 import ALL_ATTRIBUTES, Reader, Writer
 from ldap3.utils.ciDict import CaseInsensitiveWithAliasDict
-from ldap3.utils.dn import _escape_attribute_value, safe_dn, safe_rdn
+from ldap3.utils.dn import safe_dn, safe_rdn
 from ldap3.core.exceptions import LDAPKeyError
 
 from .config import cfg, ConfigAttrError
@@ -21,6 +21,75 @@ from .console import input_stderr
 from .connection import ldap
 
 log = logging.getLogger(__name__)
+
+# these constants and escape_attribute_value() taken from ldap3 library:
+# https://github.com/cannatag/ldap3
+# Copyright 2014 - 2018 Giovanni Cannata
+# Used under LGPLv3
+STATE_ANY = 0
+STATE_ESCAPE = 1
+STATE_ESCAPE_HEX = 2
+
+def escape_attribute_value(attribute_value):
+    if not attribute_value:
+        return ''
+
+    if attribute_value[0] == '#':  # with leading SHARP only pairs of hex characters are valid
+        valid_hex = True
+        if len(attribute_value) % 2 == 0:  # string must be # + HEX HEX (an odd number of chars)
+            valid_hex = False
+
+        if valid_hex:
+            for c in attribute_value:
+                if c not in hexdigits:  # allowed only hex digits as per RFC 4514
+                    valid_hex = False
+                    break
+
+        if valid_hex:
+            return attribute_value
+
+    state = STATE_ANY
+    escaped = ''
+    tmp_buffer = ''
+    for c in attribute_value:
+        if state == STATE_ANY:
+            if c == '\\':
+                state = STATE_ESCAPE
+            elif c in '"#+,;<=>\00':
+                escaped += '\\' + c
+            else:
+                escaped += c
+        elif state == STATE_ESCAPE:
+            if c in hexdigits:
+                tmp_buffer = c
+                state = STATE_ESCAPE_HEX
+            elif c in ' "#+,;<=>\\\00':
+                escaped += '\\' + c
+                state = STATE_ANY
+            else:
+                escaped += '\\\\' + c
+        elif state == STATE_ESCAPE_HEX:
+            if c in hexdigits:
+                escaped += '\\' + tmp_buffer + c
+            else:
+                escaped += '\\\\' + tmp_buffer + c
+            tmp_buffer = ''
+            state = STATE_ANY
+
+    # final state
+    if state == STATE_ESCAPE:
+        escaped += '\\\\'
+    elif state == STATE_ESCAPE_HEX:
+        escaped += '\\\\' + tmp_buffer
+
+    if escaped[0] == ' ':  # leading SPACE must be escaped
+        escaped = '\\' + escaped
+
+    # trailing SPACE must be escaped
+    if escaped[-1] == ' ' and len(escaped) > 1 and escaped[-2] != '\\':
+        escaped = escaped[:-1] + '\\ '
+
+    return escaped
 
 def entry_name(entry, name_attr):
     """Return consistent scalar entry common name."""
@@ -194,7 +263,7 @@ class LdapObjectMapping(MutableMapping):
         key = attrs[id_attr]
         if type(key) is list:
             key = key[0]
-        rdn = "=".join( [id_attr, _escape_attribute_value(key)] )
+        rdn = "=".join( [id_attr, escape_attribute_value(key)] )
         return safe_dn( [rdn, self._base] )
 
     def __iter__(self):
